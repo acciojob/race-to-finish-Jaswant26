b8495576d14ee882545bd21bac72589ebf633751
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-11-22 22:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.promises = [];
+const promises = [
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 4000 + 1000);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 4000 + 1000);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 4000 + 1000);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 4000 + 1000);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 4000 + 1000);
+  })
+];
 
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 // Do not change the code above this
 // add your promises to the array `promises`
